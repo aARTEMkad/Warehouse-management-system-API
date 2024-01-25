bca1b2da35f3a74d1c358f4be68552be1fda0aca
aARTEMkad <artemkovel2007@gmail.com>
2024-01-25 22:15:57 +0100

    Create module goods

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GoodsModule } from './goods/goods.module';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(''),
+    GoodsModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
