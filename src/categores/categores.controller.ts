f020bccade3bee7657efa9208a0e51ceeb11677c
aARTEMkad <artemkovel2007@gmail.com>
2024-01-27 00:02:39 +0100

    Writed request category

--- src/categores/categores.controller.ts
+++ src/categores/categores.controller.ts
@@ -1,32 +1,35 @@
 import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
 import { CreateCategoryDto } from './dto/createCategory.dto';
 import { UpdateCategoryDto } from './dto/updateCategory.dto';
+import { CategoresService } from './categores.service';
 
 @Controller('categores')
 export class CategoresController {
 
-    @Post()
-    createCategory(createCategoryDto: CreateCategoryDto) {
+    constructor(private categoryService: CategoresService) {    }
 
+    @Post()
+    createCategory(@Body() createCategoryDto: CreateCategoryDto) {
+        return this.categoryService.create(createCategoryDto);
     }
 
     @Get('/:id')
     findByIdCategory(@Param('id') id: string) {
-
+        return this.categoryService.findById(id);
     } 
 
     @Get()
     findByParamCategory(@Body() paramSearch: any) {
-
+        return this.categoryService.findByParam(paramSearch);
     }
 
     @Delete('/:id')
     deleteCategory(@Param('id') id: string) {
-
+        return this.categoryService.delete(id);
     }
 
     @Patch('/:id')
     updateCategory(@Param('id') id: string, @Body() updateCategoryDto: UpdateCategoryDto) {
-
+        return this.categoryService.update(id, updateCategoryDto);
     }
 }
--- src/categores/categores.service.ts
+++ src/categores/categores.service.ts
@@ -1,4 +1,34 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Category } from './Schemas/category.schema';
+import { Model } from 'mongoose';
+import { CreateCategoryDto } from './dto/createCategory.dto';
+import { UpdateCategoryDto } from './dto/updateCategory.dto';
 
 @Injectable()
-export class CategoresService {}
+export class CategoresService {
+
+    constructor(@InjectModel(Category.name) private categoryRepo: Model<Category>) {    }
+
+    create(createCategoryDto: CreateCategoryDto) {
+        const category = new this.categoryRepo(createCategoryDto);
+        return category.save();
+    }
+
+    findById(id: string) {
+        return this.categoryRepo.findById(id);
+    }
+
+    findByParam(paramSearch: any) {
+        return this.categoryRepo.find(paramSearch);
+    }
+
+    delete(id: string) {
+        return this.categoryRepo.findByIdAndDelete(id);
+    }
+
+    update(id: string, updateCategoryDto: UpdateCategoryDto) {
+        const category = this.categoryRepo.findByIdAndUpdate(id, updateCategoryDto);
+        return category;
+    }
+}
