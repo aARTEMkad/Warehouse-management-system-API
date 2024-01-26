0f0c8212817d5d5bf784021e3518df4ca8f3478d
aARTEMkad <artemkovel2007@gmail.com>
2024-01-26 23:25:41 +0100

    Create dto update and create for procuder and added request in controller

--- src/procuders/procuders.controller.ts
+++ src/procuders/procuders.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common';
+import { CreateProcuderDto } from './dto/createProcuder.dto';
+import { UpdateProducerDto } from './dto/updateProcuder.dto';
 
 @Controller('procuders')
-export class ProcudersController {}
+export class ProcudersController {
+
+    @Post()
+    createProcuder(@Body() createProcuderDto: CreateProcuderDto) {
+        
+    }
+
+    @Get('/:id')
+    findById(@Param('id') id: string) {
+
+    }
+
+    @Get()
+    findByParam(@Body() paramSearch: any) {
+
+    }
+
+    @Delete('/:id')
+    deleteProcuder(@Param('id') id: string) {
+
+    }
+
+    @Patch('/:id')
+    updateProcuder(@Param('id') id: string, @Body() updateProcuderDto: UpdateProducerDto) {
+
+    }
+
+}
